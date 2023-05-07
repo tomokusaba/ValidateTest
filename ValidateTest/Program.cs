843cac1d06b6a88756d645e6cfffd9d8fa0e3355
友光 草場 <tomokusaba@tomokusaba.onmicrosoft.com>
2023-05-07 10:06:25 +0900

    プロジェクト ファイルを追加します。

---
+++ ValidateTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ValidateTest;
+
+
+TestModel model = new()
+{
+    Name = "name",
+    Email = "fuga@hoge.com"
+};
+
+List<ValidationResult> errors = new();
+bool isValid = Validator.TryValidateObject(model, new ValidationContext(model), errors, true);
+
+if (isValid)
+{
+    Console.WriteLine("Valid");
+}
+else
+{
+    Console.WriteLine("Invalid");
+    foreach (var error in errors)
+    {
+        Console.WriteLine(error.ErrorMessage);
+    }
+}
+
---
+++ ValidateTest/TestModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidateTest
+{
+    internal class TestModel
+    {
+        [Required(ErrorMessage = "名前は必ず入力してください。")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "名前は３文字から１０文字入力してください。")]
+        public string? Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Phone]
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Zip { get; set; }
+    }
+}
